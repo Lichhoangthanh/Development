0bb83a7c058ae3129fa2cd7cb3c120530c6cd25d
Lichhoangthanh <lichhoangthanh1204@gmail.com>
2023-05-25 00:06:27 +0300

    lab 3

--- lab2/app/app.py
+++ lab2/app/app.py
@@ -1,4 +1,5 @@
 from flask import *
+import re
 
 
 app = Flask(__name__)
@@ -28,6 +29,33 @@ def cookies():
 @app.route('/form', methods=['GET', 'POST'])
 def form(): 
     return render_template('form.html')
+
+@app.route('/Telephone', methods=['GET',"POST"])
+def tel():
+    msg = ''
+    if request.method == 'POST':
+        phone_number = request.form['phone_number']
+        digits = [char for char in phone_number if char.isdigit()]
+        if digits[0]  != '8' and phone_number[0] != '+' and phone_number[1] != '7':
+            msg += 'Invalid input. Invalid number of digits. Your phone number need to start with 8 or +7 '
+            return render_template('tel.html', phone_number=phone_number, msg = msg)
+        elif   len(digits) != 11:
+            msg += 'Invalid input. Invalid number, you need to enter 11 numbers.'
+            return render_template('tel.html', phone_number=phone_number, msg = msg)
+        elif not all(char.isdigit() or char in [' ','+', '(', ')', '-', '.'] for char in phone_number):
+            msg += 'Invalid input. There are invalid characters in the phone number.'
+            return render_template('tel.html', phone_number=phone_number, msg = msg)
+        else:
+            formatted_number = '8-'
+            for i in range(1, len(digits)):
+                if i in [4,7,9]:
+                    formatted_number += '-' + ''.join(digits[i])
+                else:
+                    formatted_number += ''.join(digits[i])
+            msg = 'Accepted! Phone number entered: '
+            return render_template('tel.html', msg = msg, num = formatted_number)
+    else:
+        return render_template('tel.html')
 
 @app.route('/calc', methods=['GET', 'POST'])
 def calc():
